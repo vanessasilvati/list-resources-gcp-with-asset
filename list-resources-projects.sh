db6f990c20ae53f06969f47e667daa1ee6533290
vanessasilvati <veugenia03@gmail.com>
2021-07-14 22:55:51 -0300

    fet: Shell script para exportar inventário de recursos dos projetos na GCP

---
+++ list-resources-projects.sh
@@ -0,0 +1,17 @@
+ #!/bin/bash
+
+for PROJECT_ID in $(gcloud projects list --format="value(projectId)")
+do
+    echo projectId= $PROJECT_ID # informação adicionada para teste
+
+    if [ -z $PROJECT_ID ]
+        then
+            echo "Variables PROJECT_ID is NULL"
+        else
+            gcloud asset export \
+            --content-type resource \
+            --project ${PROJECT_ID} \
+            --asset-types compute.googleapis.com.* \
+            --output-path "gs://controle-inventario/resources-4"
+    fi
+done
